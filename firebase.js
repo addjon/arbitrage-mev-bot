c4958dff413bfb38eaa8d032a5d3f676cea67014
addjon <31444192+addjon@users.noreply.github.com>
2024-06-01 17:24:45 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 const { initializeApp } =require("firebase/app") ;
 const {getFirestore}=require('firebase/firestore');
 const firebaseConfig = {
-  apiKey: "AIzaSyD7Gg9SEvfMrCMaAgxtAHeHWp6QZj3VK9Q",
+  apiKey: "",
   authDomain: "mev-bot-42230.firebaseapp.com",
   projectId: "mev-bot-42230",
   storageBucket: "mev-bot-42230.appspot.com",
   messagingSenderId: "455140240585",
-  appId: "1:455140240585:web:a400171325bdfd02177118",
+  appId: "",
   measurementId: "G-071WZ327R6"
 };
 
